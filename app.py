a6e74d510ba655e4185c1b5b32bbdd50a08af88a
Akashsukhoi <49808660+Akashsukhoi@users.noreply.github.com>
2026-09-03 21:05:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 import time
 
 app = Flask(__name__)
@@ -11,20 +10,21 @@ def home():
 @app.route("/upload", methods=["POST"])
 def upload():
 
-    if not request.data:
-        return "No audio received", 400
-
     filename = f"received_{int(time.time())}.wav"
 
-    with open(filename, "wb") as f:
-        f.write(request.data)
+    total = 0
 
-    print(f"Received WAV: {filename}")
-    print(f"Size: {len(request.data)} bytes")
+    with open(filename, "wb") as f:
+        while True:
+            chunk = request.stream.read(4096)
+
+            if not chunk:
+                break
+
+            f.write(chunk)
+            total += len(chunk)
+
+    print(f"Received: {filename}")
+    print(f"Size: {total} bytes")
 
     return "WAV received successfully", 200
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
